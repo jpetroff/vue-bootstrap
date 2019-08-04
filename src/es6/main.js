f59c1351b0d55db8923e17876d84edcf45d81c4a
John Petrov <work.petroff@gmail.com>
2019-08-04 14:27:06 +0300

    ajax api added

--- src/es6/main.js
+++ src/es6/main.js
@@ -1,4 +1,9 @@
-import 'promise-polyfill';
-import 'whatwg-fetch';
-
+import * as utils from './utils';
 import './popup';
+
+utils.ajax({
+	url: 'https://jsonplaceholder.typicode.com/users',
+	mode: 'cors'
+})
+.then(response => { console.log(response); return response.json(); })
+.then(data => console.log(data));
